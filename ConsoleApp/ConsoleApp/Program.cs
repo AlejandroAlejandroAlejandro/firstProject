aa9816d677332fadcd4ea4e6c68f0b14dd161913
AlejandroAlejandroAlejandro <2367138@johnabbottcollege.net>
2025-03-07 01:10:14 -0500

    Created input validation

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -9,6 +9,18 @@ class Program
         string userInput;
 
         Console.Write("Enter number of students in class: ");
+
+        do
+        {
+            userInput = Console.ReadLine();
+            successfulConversion = int.TryParse(userInput, out numberOfStudents);
+
+            if (!successfulConversion || numberOfStudents <= 0)
+            {
+                Console.WriteLine("Invalid! Please try again.");
+            }
+
+        } while (!successfulConversion || numberOfStudents <= 0);
     }
 }
 
