fc95462c86658692c33da3de1faf31c55e6996b8
AlejandroAlejandroAlejandro <2367138@johnabbottcollege.net>
2025-03-18 23:58:30 -0400

    Changed the do-while loop to a while loop.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,22 +7,23 @@ class Program
 
         int numberOfStudents;
         bool successfulConversion;
-        string userInput;        
+        string userInput;
 
-        do
+
+        Console.Write("Enter number of students in class: ");
+        userInput = Console.ReadLine();
+        successfulConversion = int.TryParse(userInput, out numberOfStudents);
+
+        while (!successfulConversion || numberOfStudents <= 0)
         {
-
-            Console.Write("Enter number of students in class: ");
-
-            userInput = Console.ReadLine();
-            successfulConversion = int.TryParse(userInput, out numberOfStudents);
-
             if (!successfulConversion || numberOfStudents <= 0)
             {
                 Console.WriteLine("Invalid! Please try again.");
+                Console.Write("Enter number of students in class: ");
+                userInput = Console.ReadLine();
+                successfulConversion = int.TryParse(userInput, out numberOfStudents);
             }
-
-        } while (!successfulConversion || numberOfStudents <= 0);
+        }
 
         Console.WriteLine($"There are {numberOfStudents} students in the class.");
         Console.WriteLine("Press 'Enter' to exit...");
